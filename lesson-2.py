f633a5ca4d1f664d97c9a66c18226ff749598d98
Bakulina Daria <dbakulina0404@gmail.com>
2023-04-01 19:21:22 +0300

    working with variables

---
+++ lesson-2.py
@@ -0,0 +1,56 @@
+# print(1+1)
+
+value_1 = 4  # int
+value_2 = 0.5  # float
+# result = value_1 * value_2
+# result = value_1 / value_2
+# result = value_1 // value_2  # целочисленное деление
+# result = value_1 % value_2  # остаток от деления
+# result = value_1 ** value_2  # степень
+# print(result)
+
+# a = 2
+# b = 3
+# c = 4
+# D = b ** 2 - 4 * a * c
+
+# PEP-8 formatting Alt+Ctrl+L
+
+# active_users_count = 1000
+# price = 10
+# total_money = active_users_count * price
+
+# RATIO = 100 # умовна константа
+# print(total_momey, RATIO)
+
+# value_type = type(value_2)
+# print(value_2, value_type)
+
+# value = 123123123
+# new_value = 123123123
+# print(id(value), id(new_value))
+
+# value = "sss"
+# result = value + " " + value
+# result = value * 5
+# print(result)
+
+###################### Литералы строк #############
+
+# my_str = "New_string"
+# my_str = 'New_string'
+# my_str = """New_string"""
+# my_str = '''New_string'''
+
+# my_str = 'FC" New_string"'
+# my_str = "I'm a boy"
+
+# value = 'Dasha'
+# my_str = 'My\tname\'is\nVova'
+# new_str = r'My\tname\'is\nVova' # raw string
+# format_str = f"My value = {my_str}"
+# format_str = f"{value=}"
+
+# print(my_str)
+# print(new_str)
+# print(format_str)
